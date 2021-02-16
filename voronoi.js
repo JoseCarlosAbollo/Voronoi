bb5627e793591ac96b204367fb39c77e2b5048da
José Carlos <79134998+JoseCarlosAbollo@users.noreply.github.com>
2021-02-16 14:27:20 +0100

    Update voronoi.js

--- voronoi.js
+++ voronoi.js
@@ -1,19 +1,3 @@
-/*************************************************************************************************
-This code generates a voronoi diagram with NUM randomly generated points in a SZE*SZE image that
-can be downloaed pressing SPACE. The diagram can be made with Euclidean distance or with 
-Manhattan distance (changing comments in lines 65 and 67). To see some interaction, the points are
-drawn and when the mouse is clicked inside an area of the diagram, the nearest point is 
-highlighted.
-
-Possible improvements:
-	- It could be a function. In this case, it would be interesting being able to generate the 
-	diagram in two ways: 
-		1) from SZE and NUM {e.g. voronoi(sze,num)}
-		2) from SZE and a given array {e.g. voronoy(sze,points)}
-
-Author: Abollo Palacios, José Carlos.
-Date: 04/05/2019
-*************************************************************************************************/
 var sze = 800;		// Size of the image
 var colors = [];	// Array of colors
 var num = 70;		// Number of points
